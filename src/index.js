7556eb3a525353b66332586f5961da8008ed70f1
ChrisRast <ChrisRast@users.noreply.github.com>
2018-08-07 00:08:17 +0200

    fix line parsing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import axios from "axios";
-import _ from 'lodash-es';
 
 window.onload = () => {
-    const url = "https://nominatim.openstreetmap.org/search";
-    // const url = "https://maps.googleapis.com/maps/api/geocode/json";
+    const url = "https://nominatim.openstreetmap.org/search?";
     const params = {
         format: "json",
         countrycode: 'tn'
-        // region: 'tn',
-        // key: 'AIzaSyBhNOzK8XBVvqnzfZKu9_3vTKsEXsRXGAs'
     };
     const download = document.getElementById('download');
     const status = document.getElementById('status');
@@ -17,28 +13,28 @@ window.onload = () => {
         status.innerText = 'Parsing file...';
         const reader = new FileReader();
         reader.onload = (readerEvent) => {
-            const data = readerEvent.target.result.split('\r\n');
+            const data = readerEvent.target.result.split('\n');
             status.innerText = 'Fetching data...';
             axios.all(data.map((line) => {
                 return axios.get(url, {
                     params: {
                         ...params,
                         q: `${line}, Tunisia`
-                        // address: `${line}, Tunisia`
                     }
                 })
-            })).then(result => {
+            }))
+            .then(result => {
                 status.innerText = 'Creating CSV...';
 
                 let countNA = 0;
                 let csv = result.map((aResult, index) => {
                     const label = data[ index ];
-                    // const location = _.get(aResult, 'data.results.0.geometry.location', {});
-                    const location = _.get(aResult, 'data.0', {});
-                    const {
-                        lat = 'NA',
-                        lon: lng = 'NA',
-                    } = location;
+                    const [
+                        {
+                            lat = 'NA',
+                            lon: lng = 'NA',
+                        } = {}
+                    ] = aResult.data;
                     if (lat === 'NA' || lng === 'NA') countNA++;
 
                     return `\n${label},${lat},${lng}`;
