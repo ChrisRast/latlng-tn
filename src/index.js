ee6fb72f0f380548bc0eecc3d5c3b6999556029d
ChrisRast <ChrisRast@users.noreply.github.com>
2018-08-08 21:42:35 +0200

    throttle and better config

--- src/index.js
+++ src/index.js
@@ -1,63 +1,103 @@
 import axios from "axios";
 
 window.onload = () => {
-    const url = "https://nominatim.openstreetmap.org/search?";
-    const params = {
-        format: "json",
-        countrycode: 'tn'
-    };
-    const download = document.getElementById('download');
-    const status = document.getElementById('status');
-    download.onclick = (fetchElemEvent) => {
-        fetchElemEvent.preventDefault();
-        status.innerText = 'Parsing file...';
-        const reader = new FileReader();
-        reader.onload = (readerEvent) => {
-            const data = readerEvent.target.result.split('\n');
-            status.innerText = 'Fetching data...';
-            axios.all(data.map((line) => {
-                return axios.get(url, {
-                    params: {
-                        ...params,
-                        q: `${line}, Tunisia`
-                    }
-                })
-            }))
-            .then(result => {
-                status.innerText = 'Creating CSV...';
+	const url = "https://eu1.locationiq.com/v1/search.php?";
+	const params = {
+		format: "json",
+		countrycode: 'tn',
+	};
+	const parse = document.getElementById('parse');
+	const download = document.getElementById('download');
+	const status = document.getElementById('status');
+	const fileInput = document.getElementById('input');
+	const key = document.getElementById('key');
+	const rate = document.getElementById('rate');
 
-                let countNA = 0;
-                let csv = result.map((aResult, index) => {
-                    const label = data[ index ];
-                    const [
-                        {
-                            lat = 'NA',
-                            lon: lng = 'NA',
-                        } = {}
-                    ] = aResult.data;
-                    if (lat === 'NA' || lng === 'NA') countNA++;
+	fileInput.onchange = () => {
+		if (fileInput.files[ 0 ] && key) {
+			parse.disabled = false;
+		}
+	}
+	parse.onclick = (fetchElemEvent) => {
+		fetchElemEvent.preventDefault();
+		status.innerText = 'Parsing file...';
+		const reader = new FileReader();
+		reader.onload = (readerEvent) => {
+			let data = [];
+			try {
+				data = readerEvent.target.result.split('\n');
+			} catch (error) {
+				status.innerText = error;
+				throw new Error(error);
+			}
 
-                    return `\n${label},${lat},${lng}`;
-                });
+			status.innerText = `Fetching data... 0/${data.length}`;
+			let firstLineNbValues = null;
+			const result = [];
+			let index = 0;
+			const interval = setInterval(sendRequest, (60 / rate.value * 1000))
+			let countNA = 0;
 
-                csv.unshift('lbgouver,lbdeleg,lat,lng');
-                const finalTxt = csv.join('');
-                download.onclick = (dlElemEvent) => {
-                    dlElemEvent.target.href = 'data:text/plain;charset=utf-8,'
-                        + encodeURIComponent(finalTxt);
-                };
-                status.innerText = 'Finished';
+			const callback = () => {
+				status.innerText = 'Creating CSV...';
 
-                download.innerText = 'Now download the file!';
+				let csv = result.map((aResult, index) => {
+					const label = data[ index ];
+					const [
+						{
+							lat = 'NA',
+							lon: lng = 'NA',
+						} = {}
+					] = aResult.data;
+					if (lat === 'NA' || lng === 'NA') countNA++;
 
-                document.getElementById('result').innerText = finalTxt;
-                status.innerText = `${countNA} geolocations failed to be found. Check the generated file :-)`;
-            }).catch(error => {
-                console.log("error", error);
-            });
-        };
-        reader.readAsText(document.getElementById('input').files[ 0 ]);
-    };
+					return `\n${label},${lat},${lng}`;
+				});
+
+				csv.unshift(`${Array(firstLineNbValues).join(',')},lat,lng`);
+				const finalTxt = csv.join('');
+				download.onclick = (dlElemEvent) => {
+					dlElemEvent.target.href = 'data:text/plain;charset=utf-8,'
+						+ encodeURIComponent(finalTxt);
+				};
+				status.innerText = 'Finished';
+
+				download.innerText = 'Now download the file!';
+
+				document.getElementById('result').innerText = finalTxt;
+				status.innerText = countNA ? `❌ ${countNA} lines in ${data.length} failed to be geocoded.` : '✔ All lines with content were geocoded.';
+			}
+			function sendRequest() {
+				status.innerText = `Fetching data... ${index + 1}/${data.length}`;
+				const line = data[ index ];
+				if (index === 0) {
+					firstLineNbValues = line.split(',').length
+				}
+				if (index + 1 === data.length) clearInterval(interval);
+				index++
+				if (line.length) {
+					axios.get(url, {
+						params: {
+							...params,
+							q: line,
+							key: key.value
+						}
+					}).then((res) => {
+						result.push(res);
+					}).catch((error) => {
+						countNA++;
+					}).finally(() => {
+						if (index === data.length) {
+							callback();
+						}
+					});
+				} else if (index === data.length) {
+					callback();
+				}
+			}
+		};
+		reader.readAsText(fileInput.files[ 0 ]);
+	};
 }
 
 
